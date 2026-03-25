4f29f2b8d5686fac37be2e4febf7b9b5d6df7f28
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-25 11:39:20 +0200

    const

--- js/task-1.js
+++ js/task-1.js
@@ -29,33 +29,93 @@
 // console.log(customer.getBalance()); // 19750
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
-class Car {
-  #price;
-  static maxPrice = 50000;
+/**
+ * Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю і
+ * розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
+ * кількістю з властивості stones.
+ */
 
-  constructor(params) {
-    this.#price = params.price;
-  }
+const chopShop = {
+  stones: [
+    { name: 'Emerald', price: 1300, quantity: 4 },
+    { name: 'Diamond', price: 2700, quantity: 3 },
+    { name: 'Sapphire', price: 1400, quantity: 7 },
+    { name: 'Ruby', price: 800, quantity: 2 },
+  ],
+  calcTotalPrice(stoneName) {
+    // const stone = this.stones.find((item) => item.name === stoneName);
+    // //      undefined => false => !false => true
+    // if(!stone) {
+    //     return `${stoneName} not found`;
+    // } else {
+    //     return stone.price * stone.quantity;
+    // }
+  },
+};
 
-  get price() {
-    return this.#price;
-  }
+console.log(chopShop.calcTotalPrice('lalala'));
 
-  set price(newPrice) {
-    this.#price = newPrice;
-    if (newPrice > maxPrice) {
-      console.log(newPrice);
-    } else {
-      newPrice = maxPrice;
-    }
-  }
-}
+/**
+ * ----------------------------------------
+ */
+// Наше завдання написати програмне забезпечення для автомобіля, а саме натискання кнопки набору та зниження швидкості в системі круїз контролю.
+// Створити об'єкт cruiseControl з методами accelerate та decrease, властивостями speed та brand.
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+// const cruiseControl = {
+//     speed: 0,
+//     brand: "Audi",
+//     accelerate() {
+//         this.speed += 10;
+//         console.log(`Автомобіль ${this.brand} прискорюється. Швидкість ${this.speed}`);
+//     },
+//     decrease() {
+//         if(this.speed <= 0) {
+//             console.log("Авто зупинилось");
+//             return;
+//         }
 
-audi.price = 49000;
-console.log(audi.price); // 49000
+//         this.speed -= 10;
+//         console.log(`Автомобіль ${this.brand} гальмує. Швидкість ${this.speed}`);
+//     }
+// }
 
-audi.price = 51000;
-console.log(audi.price); // 49000
+// cruiseControl.accelerate();
+// cruiseControl.accelerate();
+// cruiseControl.accelerate();
+
+// cruiseControl.decrease();
+// cruiseControl.decrease();
+// cruiseControl.decrease();
+// cruiseControl.decrease();
+
+// console.log(cruiseControl);
+
+// Потрібно створити функціонал для контролю швидкості прокатних авто.
+// Створіть функцію яка буде приймати 1 параметр (максимально дозволену швидкість)
+// та виводити повідомлення, чи ми рухаємось з безпечною швидкістю чи перевищуємо, функція має опрацьовувати об'єкт автомобіля як this
+
+// const SPEED = 60;
+
+// const bmw = {
+//     brand: "bmw",
+//     speed: 30
+// }
+
+// const audi = {
+//     brand: "audi",
+//     speed: 70
+// }
+
+// function speedSensor(maxSpeed) {
+//     if(this.speed <= maxSpeed) {
+//         return `Авто ${this.brand} рухаєтсья з безпечною швидкістю`
+//     } else {
+//         return `Авто ${this.brand} перевищує`
+//     }
+// }
+
+// console.log(speedSensor.call(bmw, SPEED));
+
+// const audiSppedSensor = speedSensor.bind(audi);
+
+// console.log(audiSppedSensor(SPEED));
