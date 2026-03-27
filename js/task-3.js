e79af9e021e66788bedf22f2e21e86da7e5ad6b2
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-27 11:53:14 +0200

    task

--- js/task-3.js
+++ js/task-3.js
@@ -6,3 +6,23 @@
 // console.log(builder.getValue()); // "^.^"
 // builder.padBoth('=');
 // console.log(builder.getValue()); // "=^.^="
+
+class Car {
+  static #maxPrice = 50000;
+
+  constructor(params) {
+    this.price = params.price;
+  }
+  checkPrice(price) {
+    if (price > Car.#maxPrice) {
+      return 'Error! Price exceeds the maximum';
+    }
+    return 'Success! Price is within acceptable limits';
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
